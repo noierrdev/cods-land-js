e9feff7326b98a4b59cb9377c5e62e25cf725abd
TimMark <noierrdev@proton.me>
2023-12-06 07:13:15 -0800

    on the router, buxfied

--- routers/appointments.router.js
+++ routers/appointments.router.js
@@ -2,8 +2,7 @@ const router=require('express').Router()
 const appointmentsController=require('../controllers/appointments.controller');
 router.get("/",(req,res)=>res.json({status:"success"}));
 
-router.post('/events/save',appointmentsController.saveEvent);
-router.get('/events/',appointmentsController.allEvents);
+
 
 router.post('/appointment-types/save',appointmentsController.saveAppointmentType);
 router.get('/appointment-types/',appointmentsController.allAppointmentTypes);
