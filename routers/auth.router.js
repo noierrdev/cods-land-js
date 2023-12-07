87f0ceae935b0000d3aa39480920c54de1578b91
TimMark <noierrdev@proton.me>
2023-12-07 08:27:38 -0800

    verify router added

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -6,6 +6,7 @@ router.get('/',authController.verify);
 router.post('/signup',authController.signup);
 router.post('/signin',authController.signin);
 router.get('/signout',authController.signout);
+router.use('/verify',authController.verify)
 router.post('/forgot-password',authController.forgotPassword)
 router.post('/reset-password',authController.resetPassword)
 router.post('/verify-otp',authController.verifyOTP)
