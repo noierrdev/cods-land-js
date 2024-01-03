5e3643bbcf7a89c098f6419ecc4d3a31613b53bb
TimMark <noierrdev@proton.me>
2024-01-03 15:57:25 -0800

    edit cart

--- routers/shop.router.js
+++ routers/shop.router.js
@@ -4,9 +4,9 @@ router.get("/",(req,res)=>{
     return res.json({status:"success"})
 });
 router.post('/categories',shopController.saveCategory);
-router.get('/categories/all',shopController.allCategories)
+router.get('/categories/',shopController.allCategories)
 router.delete('/categories/:category_id',shopController.deleteCategory);
-router.post('/categories/:category_id/page',shopController.categoryPage);
+router.post('/categories/:category_id/products/page',shopController.categoryPage);
 router.post('/products',shopController.saveProduct);
 router.post('/products/page',shopController.productsPage);
 router.delete('/products/:product_id',shopController.deleteProduct)
