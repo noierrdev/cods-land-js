ff854911ab01aa7fe2d9c258ff84a1beea20bf95
moon920830 <lucasdavis0830@gmail.com>
2024-02-12 13:52:49 -0800

    shipo

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -422,7 +422,7 @@ exports.shipOrder=async (req, res) =>{
             });
         }
     
-        var rate = shipment.rates[0];
+        var rate = shipment.rates[1];
         shippo.transaction.create({
             "rate": rate.object_id,
             "label_file_type": "PDF",
