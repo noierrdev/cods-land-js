f3e18d240c0d1a3d761d1e8c6dd16919b12fb12f
Vander Moleker <noierdev@proton.me>
2024-03-26 18:45:15 +0100

    product info

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -49,7 +49,22 @@ exports.allCategories=(req,res)=>{
 exports.productsPage=(req,res)=>{
     const page=req.body.page;
     const pagesize=req.body.pagesize;
-    models.Product.find({},{title:true,image_url:true,description:true,createdAt:true,price:true,count:true,}).skip(page*pagesize).limit(pagesize).populate('category category_1 category_2 category_3','title')
+    models.Product.find({},{
+        title:true,
+        image_url:true,
+        description:true,
+        createdAt:true,
+        price:true,
+        count:true,
+        category_1:true,
+        category_2:true,
+        category_3:true,
+        public:true,
+        width:true,
+        length:true,
+        height:true,
+        weight:true
+    }).skip(page*pagesize).limit(pagesize).populate('category category_1 category_2 category_3','title')
     .then(async gotProducts=>{
         const totalNumbers=await models.Product.countDocuments().lean().exec();
         const total=Math.ceil(totalNumbers/pagesize);
