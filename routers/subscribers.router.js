f3d4489a92234d40089c14a356b642910f166726
Vander Moleker <noierdev@proton.me>
2024-04-26 21:20:15 +0200

    subscriber

--- routers/subscribers.router.js
+++ routers/subscribers.router.js
@@ -3,6 +3,7 @@ const subscribersController=require("../controllers/subscribers.controller")
 router.post("/",subscribersController.saveSubscriber);
 router.post("/page",subscribersController.pageSubscribers);
 router.post("/upload",subscribersController.uploadCSV);
+router.delete("/all",subscribersController.deleteAll);
 router.delete("/:id",subscribersController.deleteSubscribers);
 
 module.exports=router;
