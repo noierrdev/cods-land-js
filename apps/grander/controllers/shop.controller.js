1602fd12f83bd5bd1236ed4b65f07c022d6d5785
Vander Moleker <noierdev@proton.me>
2024-04-26 11:06:03 +0200

    remain grander orderiung

--- apps/grander/controllers/shop.controller.js
+++ apps/grander/controllers/shop.controller.js
@@ -4,145 +4,113 @@ const stripe=require('stripe')(process.env.STRIPE_KEY)
 var shippo = require('shippo')(process.env.SHIPPO_KEY);
 const path=require('path')
 
+// exports.pageGranderProducts=(req,res)=>{
+//     const page=req.body.page;
+//     const pagesize=req.body.pagesize;
+//     const category=req.body.category;
+//     const search=req.body.search;
+//     const searchFilter=new RegExp(search,"i");
+//     var filter={};
+//     if(category) filter={
+//         ...filter,
+//         $or:[
+//             {category_1:category},
+//             {category_2:category},
+//             {category_3:category}
+//         ]
+//     }
+//     if(search) filter={
+//         ...filter,
+//         $or:[
+//             {title:searchFilter},
+//             {description:searchFilter}
+//         ]
+//     }
+//     models.Product.find({},{
+//         title:true,
+//         image_url:true,
+//         description:true,
+//         createdAt:true,
+//         product_no:true,
+//         price:true,
+//         category:true,
+//     }).skip(page*pagesize).limit(pagesize)
+//     .then(async gotProducts=>{
+//         const totalNumbers=await models.Product.countDocuments({}).lean().exec();
+//         const total=Math.ceil(totalNumbers/pagesize);
+//         return res.json({status:"success",data:{
+//             pagedata:gotProducts,
+//             page,
+//             pagesize,
+//             totalNumbers,
+//             total
+//         }})
+//     })
+// }
+// exports.saveGranderProduct=(req,res)=>{
+//     const title=req.body.title;
+//     const description=req.body.description;
+//     const price=req.body.price;
+//     const sugg_retail=req.body.sugg_retail;
+//     const reseller_price=req.body.reseller_price;
+//     const image_url=req.body.image_url;
+//     const image=req.files?req.files.image:null;
+//     const product_no=req.body.product_no;
+//     const category=req.body.category;
+//     const public=req.body.public;
+//     const newProduct=new grander_models.GranderProduct({
+//         title,
+//         description,
+//         price,
+//         sugg_retail,
+//         reseller_price,
+//         image_url,
+//         image,
+//         product_no,
+//         category,
+//         public
+//     });
+//     newProduct.save()
+//     .then(()=>res.json({status:"success"}))
+//     .catch((e)=>res.json({status:"error",error:e}))
+// }
+// exports.deleteGranderProduct=(req,res)=>{
+//     const id=req.params.id;
+//     grander_models.GranderProduct.findByIdAndRemove(id)
+//     .then(()=>res.json({status:"success"}))
+//     .catch((e)=>res.json({status:"success",error:e}))
+// }
+// exports.updateGranderProduct=(req,res)=>{
+//     const id=req.params.id;
+//     const {...data}=req.body;
+//     grander_models.GranderProduct.findByIdAndUpdate(id,{
+//         $set:{
+//             ...data
+//         }
+//     })
+//     .then(()=>res.json({status:"success"}))
+//     .catch(e=>res.json({status:"error",error:e}))
+// }
+// exports.getGranderProduct=(req,res)=>{
+//     const id=req.params.id;
+//     grander_models.GranderProduct.findById(id)
+//     .then(gotProduct=>{
+//         return res.json({status:'success',data:gotProduct})
+//     })
+//     .catch(e=>res.json({status:"error",error:e}))
+// }
 
-exports.pageProducts=(req,res)=>{
-    const page=req.body.page;
-    const pagesize=req.body.pagesize;
-    models.Product.find({},{
-        title:true,
-        image_url:true,
-        description:true,
-        createdAt:true,
-        price:true,
-        count:true,
-        category_1:true,
-        category_2:true,
-        category_3:true,
-        public:true,
-        width:true,
-        length:true,
-        height:true,
-        weight:true
-    }).skip(page*pagesize).limit(pagesize).populate("category_1 category_2 category_3","title")
-    .then(async gotProducts=>{
-        const totalNumbers=await models.Product.countDocuments({}).lean().exec();
-        const total=Math.ceil(totalNumbers/pagesize);
-        return res.json({status:"success",data:{
-            pagedata:gotProducts,
-            page,
-            pagesize,
-            totalNumbers,
-            total
-        }})
-    })
-}
-exports.pageGranderProducts=(req,res)=>{
-    const page=req.body.page;
-    const pagesize=req.body.pagesize;
-    grander_models.GranderProduct.find({public:true},{
-        title:true,
-        image_url:true,
-        description:true,
-        createdAt:true,
-        product_no:true,
-        price:true,
-        category:true,
-    }).skip(page*pagesize).limit(pagesize)
-    .then(async gotProducts=>{
-        const totalNumbers=await grander_models.GranderProduct.countDocuments({public:true}).lean().exec();
-        const total=Math.ceil(totalNumbers/pagesize);
-        return res.json({status:"success",data:{
-            pagedata:gotProducts,
-            page,
-            pagesize,
-            totalNumbers,
-            total
-        }})
-    })
-}
-exports.saveGranderProduct=(req,res)=>{
-    const title=req.body.title;
-    const description=req.body.description;
-    const price=req.body.price;
-    const sugg_retail=req.body.sugg_retail;
-    const reseller_price=req.body.reseller_price;
-    const image_url=req.body.image_url;
-    const image=req.files?req.files.image:null;
-    const product_no=req.body.product_no;
-    const category=req.body.category;
-    const public=req.body.public;
-    const newProduct=new grander_models.GranderProduct({
-        title,
-        description,
-        price,
-        sugg_retail,
-        reseller_price,
-        image_url,
-        image,
-        product_no,
-        category,
-        public
-    });
-    newProduct.save()
-    .then(()=>res.json({status:"success"}))
-    .catch((e)=>res.json({status:"error",error:e}))
-}
-exports.deleteGranderProduct=(req,res)=>{
-    const id=req.params.id;
-    grander_models.GranderProduct.findByIdAndRemove(id)
-    .then(()=>res.json({status:"success"}))
-    .catch((e)=>res.json({status:"success",error:e}))
-}
-exports.updateGranderProduct=(req,res)=>{
-    const id=req.params.id;
-    const {...data}=req.body;
-    grander_models.GranderProduct.findByIdAndUpdate(id,{
-        $set:{
-            ...data
-        }
-    })
-    .then(()=>res.json({status:"success"}))
-    .catch(e=>res.json({status:"error",error:e}))
-}
-exports.getGranderProduct=(req,res)=>{
-    const id=req.params.id;
-    grander_models.GranderProduct.findById(id)
-    .then(gotProduct=>{
-        return res.json({status:'success',data:gotProduct})
-    })
-    .catch(e=>res.json({status:"error",error:e}))
-}
+// exports.getGranderProductImage=(req,res)=>{
+//     const product=req.params.id;
+//     models.GranderProduct.findById(product,{image:true})
+//     .then(gotImage=>{
+//         if(!gotImage.image) return res.setHeader("Content-Type","image/png").sendFile(path.resolve(__dirname,"../../../static/images/default-product.png"))
+//         return res.setHeader("Content-Type",gotImage.image.mimetype).send(gotImage.image.data.buffer);
+//     })
+//     .catch(e=>res.json({status:"error",error:e}))
+// }
 
-exports.getGranderProductImage=(req,res)=>{
-    const product=req.params.id;
-    models.GranderProduct.findById(product,{image:true})
-    .then(gotImage=>{
-        if(!gotImage.image) return res.setHeader("Content-Type","image/png").sendFile(path.resolve(__dirname,"../../../static/images/default-product.png"))
-        return res.setHeader("Content-Type",gotImage.image.mimetype).send(gotImage.image.data.buffer);
-    })
-    .catch(e=>res.json({status:"error",error:e}))
-}
 
-exports.saveOrder=(req,res)=>{
-    
-}
-exports.pageOrders=(req,res)=>{
-    const page=req.body.page;
-    const pagesize=req.body.pagesize;
-    models.Order.find().populate('user products.product','fullname email title description price image_url').skip(page*pagesize).limit(pagesize).lean().exec()
-    .then(async gotOrders=>{
-        const totalNumbers=await models.Order.countDocuments();
-        const total=Math.ceil(totalNumbers/pagesize)
-        return res.json({status:"success",data:{
-            pagedata:gotOrders,
-            page:page,
-            pagesize:pagesize,
-            total,
-            totalNumbers
-        }})
-    })
-    .catch(e=>res.json({status:"error",error:"DB_ERROR"}))
-}
 exports.pageGranderOrders=(req,res)=>{
     const page=req.body.page;
     const pagesize=req.body.pagesize;
@@ -335,7 +303,7 @@ exports.sendShippingRequest=(req,res)=>{
 
 exports.purchase=(req,res)=>{
     const order_id=req.body.order;
-    models.Order.findByIdAndUpdate(order_id,{$set:{purchase_info:req.body,paid:true}})
+    grander_models.GranderOrder.findByIdAndUpdate(order_id,{$set:{purchase_info:req.body,paid:true}})
     .then(()=>{
         return res.json({status:"success"})
     })
--- apps/grander/routers/shop.router.js
+++ apps/grander/routers/shop.router.js
@@ -5,16 +5,16 @@ router.get("/",(req,res)=>{
     res.json({status:"success",data:"API of theessenceoflife.com"});
 })
 
-router.post("/products/page",shopController.pageGranderProducts);
-router.post("/products",shopController.saveGranderProduct);
-router.delete("/products/:id",shopController.deleteGranderProduct);
-router.put("/products/:id",shopController.updateGranderProduct);
-router.get("/products/:id/image",shopController.getGranderProductImage)
-router.get("/products/:id",shopController.getGranderProduct);
+// router.post("/products/page",shopController.pageGranderProducts);
+// router.post("/products",shopController.saveGranderProduct);
+// router.delete("/products/:id",shopController.deleteGranderProduct);
+// router.put("/products/:id",shopController.updateGranderProduct);
+// router.get("/products/:id/image",shopController.getGranderProductImage)
+// router.get("/products/:id",shopController.getGranderProduct);
 
 
 router.post("/orders",shopController.saveGranderOrder);
-router.post("/orders/page",shopController.pageOrders);
+router.post("/orders/page",shopController.pageGranderOrders);
 router.post('/orders/purchase',shopController.purchase);
 router.put("/orders/:id",shopController.selectShippingRate);
 router.post("/orders/:id",shopController.sendShippingRequest);
