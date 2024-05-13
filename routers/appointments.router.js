0f387e84b7239702da1024a545175cd226980ae6
Vander Moleker <noierdev@proton.me>
2024-05-13 06:58:12 +0200

    added route

--- routers/appointments.router.js
+++ routers/appointments.router.js
@@ -7,10 +7,16 @@ router.get('/appointment-types/',appointmentsController.allAppointmentTypes);
 
 router.post('/save',appointmentsController.saveAppointment);
 router.get('/my',appointmentsController.allAppointments)
+router.get('/page',appointmentsController.pageAppointment)
 router.get('/:id',appointmentsController.getAppointment)
+router.delete('/:id',appointmentsController.deleteAppointment)
 router.get('/:id/complete',appointmentsController.completeAppointment)
 
 
 router.post("/calendar",appointmentsController.getFromRange)
+
+router.post("/meetings",appointmentsController.saveMeeting)
+router.post("/meetings/page",appointmentsController.pageMeetings)
+router.delete("/meetings/:id",appointmentsController.deleteMeeting)
 
 module.exports=router;
