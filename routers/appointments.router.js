d0aa91161ab80ccaef6e55350c1b7d6f0eba4186
Vander Moleker <noierdev@proton.me>
2024-05-22 14:14:35 +0200

    added start payment into appointment controller

--- routers/appointments.router.js
+++ routers/appointments.router.js
@@ -1,25 +1,31 @@
 const router=require('express').Router()
 const appointmentsController=require('../controllers/appointments.controller');
 router.get("/",(req,res)=>res.json({status:"success"}));
+//Start payment
 router.post('/start-payment',appointmentsController.startPayment)
+
+//Appointment-Types management
 router.post('/appointment-types/save',appointmentsController.saveAppointmentType);
 router.get('/appointment-types/',appointmentsController.allAppointmentTypes);
 
+//Appointment-Events management
 router.post('/events', appointmentsController.saveAppointmentEvent);
 router.post('/events/page', appointmentsController.pageAppointmentEvents);
 router.delete('/events/:id', appointmentsController.deleteAppointmentEvent);
 router.post('/events/vaild',appointmentsController.vaildateDate)
 
 
-router.post("/calendar",appointmentsController.getFromRange)
 
+//Alternative solution
 router.post("/meetings",appointmentsController.saveMeeting)
 router.post("/meetings/page",appointmentsController.pageMeetings)
 router.delete("/meetings/:id",appointmentsController.deleteMeeting)
 
+//Appointments
 router.post('/save',appointmentsController.newSaveAppointment);
 router.get('/my',appointmentsController.allAppointments)
 router.post('/page',appointmentsController.pageAppointment)
+router.post("/calendar",appointmentsController.getFromRange)
 router.put('/:id/accept',appointmentsController.acceptAppointment)
 router.put('/:id/cancel',appointmentsController.cancelAppointment)
 router.get('/:id/complete',appointmentsController.completeAppointment)
